56ae04b71541d3e16445cdb68fd7e4864b1a2ad3
francisco.amorim <francisco.amorim@iugome.com>
2021-06-09 19:41:27 -0700

    Open and display PDF files
    Followed the pdftron.com documentation guide to integrate the PDFTron UWP SDK.

--- PDFReader/PDFReader/MainPage.xaml.cs
+++ PDFReader/PDFReader/MainPage.xaml.cs
@@ -12,6 +12,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.Storage;
+using Windows.Storage.Pickers;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +24,33 @@ namespace PDFReader
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        pdftron.PDF.PDFViewCtrl MyPDFViewCtrl;
+
         public MainPage()
         {
             this.InitializeComponent();
+            pdftron.PDFNet.Initialize("your_key");
+            MyPDFViewCtrl = new pdftron.PDF.PDFViewCtrl();
+            PDFViewBorder.Child = MyPDFViewCtrl;
+            OpenButton.Click += OpenButton_Click;
+        }
+
+        async void OpenButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Get a file from the file picker.
+            FileOpenPicker fileOpenPicker = new FileOpenPicker();
+            fileOpenPicker.ViewMode = PickerViewMode.List;
+            fileOpenPicker.FileTypeFilter.Add(".pdf");
+            StorageFile file = await fileOpenPicker.PickSingleFileAsync();
+
+            // Create a PDFDocument and use it as the source for the PDFViewCtrl
+            if (file != null)
+            {
+                Windows.Storage.Streams.IRandomAccessStream stream = await
+                    file.OpenAsync(FileAccessMode.ReadWrite);
+                pdftron.PDF.PDFDoc doc = new pdftron.PDF.PDFDoc(stream);
+                MyPDFViewCtrl.SetDoc(doc);
+            }
         }
     }
 }
